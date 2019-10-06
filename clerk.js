88c6676c10cbfd723bc556802e8cc4f369ad2522
Eric Stallings <stallings.eric@gmail.com>
2019-10-06 11:28:01 -0700

    Added Alt Answers

--- clerk.js
+++ clerk.js
@@ -48,8 +48,7 @@ function tickets(peopleInLine){
       }
 
     }
-  }
-  )
+  })
   
 return answer;
 
@@ -80,4 +79,72 @@ for (let i = 0; i < peopleInLine.length; i++){
       }
     }
   }
+*/
+
+/*
+Alt Answers: 
+1. 
+ function Clerk(name) {
+  this.name = name;
+  
+  this.money = {
+    25 : 0,
+    50 : 0,
+    100: 0 
+  };
+  
+  this.sell = function(element, index, array) {
+    this.money[element]++;
+
+    switch (element) {
+      case 25:
+        return true;
+      case 50:
+        this.money[25]--;
+        break;
+      case 100:
+        this.money[50] ? this.money[50]-- : this.money[25] -= 2;
+        this.money[25]--;
+        break;
+    }
+    return this.money[25] >= 0;
+  };
+}
+
+function tickets(peopleInLine){
+  var vasya = new Clerk("Vasya");
+  return peopleInLine.every(vasya.sell.bind(vasya)) ? "YES" : "NO";
+}
+
+*/
+
+/* 
+#2. 
+
+function tickets(peopleInLine) {
+  var bills = [0, 0, 0]
+  for (var i = 0; i < peopleInLine.length; i++) {
+    switch (peopleInLine[i]) {
+      case 25:
+        bills[0]++
+        break
+        
+      case 50:
+        bills[0]--
+        bills[1]++
+        break
+        
+      case 100:
+        bills[1] ? bills[1]-- : bills[0] -= 2
+        bills[0]--
+        break
+    }
+    
+    if (bills[0] < 0) {
+      return 'NO'
+    }
+  }
+  
+  return 'YES'
+}
 */
