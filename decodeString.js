1e166a90311342cfd2a369447d81c48bc443c546
Eric Stallings <stallings.eric@gmail.com>
2019-10-18 09:37:35 -0700

    Hack Hour 10/18

---
+++ decodeString.js
@@ -0,0 +1,14 @@
+/*
+This is a simple string decoding algorithm. The idea is to take a string of characters and decode it into an array. Each character is a single element in the result unless a backslash followed by a number is encountered.
+
+When a backslash followed by a number is found, the number indicates how many of the next characters are grouped together as one element.
+
+Example:
+
+'abc\5defghi\2jkl' => ['a', 'b', 'c', 'defgh', 'i', 'jk', 'l']
+If the number is larger than the count of remaining character treat it as reading the remaining characters.
+
+If you are reading characters, and you find an escape inside a string, they should be tallied into the string:
+
+'\5ab\3cde' => ['ab\3c', 'd', 'e'] 
+*/
